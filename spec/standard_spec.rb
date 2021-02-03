bc469810190a08e85dda6a05b6034e128bc13664
MZiserman <martinziserman@gmail.com>
2021-02-03 12:25:43 +0100

    deferred / trimestrial

--- spec/standard_spec.rb
+++ spec/standard_spec.rb
@@ -16,17 +16,96 @@ RSpec.describe LoanGenerator::Standard do
   let(:months_per_term) { 1 }
   let(:deferred) { 0 }
 
-  context 'with default values' do
-    it "initializes" do
-      expect { loan }.not_to raise_error
+  it "initializes" do
+    expect { loan }.not_to raise_error
+  end
+
+  it 'has right calculated term rate' do
+    expect(loan.rate_per_term).to be_within(0.00004).of(0.0095)
+  end
+
+  it 'has right payment per term' do
+    expect(loan.payment_per_term).to be_within(0.004).of(88562.07)
+  end
+
+  it 'reimburse all capital' do
+    expect(loan.time_tables.map(&:capital_part).sum).to be_within(0.004).of(
+      capital
+    )
+  end
+
+  context 'with deferred' do
+    let(:deferred) { 6 }
+
+    let(:model) do
+      described_class.new(
+        capital: capital,
+        duration: 6,
+        rate: rate,
+        months_per_term: months_per_term,
+        deferred: 0,
+      )
     end
 
     it 'has right calculated term rate' do
-      expect(loan.rate_per_term).to be_within(0.0005).of(0.009)
+      expect(loan.rate_per_term).to be_within(0.00004).of(0.0095)
     end
 
-    it 'has right payment per term' do
-      expect(loan.payment_per_term).to be_within(0.1).of(88562.07)
+    it 'reimburse all capital' do
+      expect(loan.time_tables.map(&:capital_part).sum).to be_within(0.004).of(
+        capital
+      )
+    end
+
+    it 'has right calculated term rate' do
+      expect(loan.payment_per_term).to eq(model.payment_per_term)
+    end
+  end
+
+  context 'with 3 months per terms' do
+    let(:months_per_term) { 3 }
+    let(:duration) { 4 }
+
+    it 'has right calculated term rate' do
+      expect(loan.rate_per_term).to be_within(0.00004).of(0.0287)
+    end
+
+    it 'reimburse all capital' do
+      expect(loan.time_tables.map(&:capital_part).sum).to be_within(0.004).of(
+        capital
+      )
+    end
+
+    it 'has right calculated payment per term' do
+      expect(loan.payment_per_term).to be_within(0.004).of(268215.22)
+    end
+
+    context 'with deferred' do
+      let(:deferred) { 2 }
+
+      let(:model) do
+        described_class.new(
+          capital: capital,
+          duration: 2,
+          rate: rate,
+          months_per_term: months_per_term,
+          deferred: 0,
+        )
+      end
+
+      it 'has right calculated term rate' do
+        expect(loan.rate_per_term).to be_within(0.00004).of(0.0287)
+      end
+
+      it 'reimburse all capital' do
+        expect(loan.time_tables.map(&:capital_part).sum).to be_within(0.004).of(
+          capital
+        )
+      end
+
+      it 'has right calculated term rate' do
+        expect(loan.payment_per_term).to eq(model.payment_per_term)
+      end
     end
   end
 end
